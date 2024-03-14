83daf61a3186884b4205f1ee0b8da2d0a02d4333
pablogescobar <pablofiloc@gmail.com>
2024-03-14 19:23:47 -0300

    Agregar archivos en el directorio principal

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+const { Server } = require('socket.io');
+const homeRouter = require('./routes/home.router');
+const realTimeProductsRouter = require('./routes/realTimeProducts.router')
+const app = express();
+
+
+// Configuración de handlebars
+app.engine('handlebars', handlebars.engine());
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(`${__dirname}/../public`));
+
+//  envío de información mediante formularios y JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/api/home', homeRouter)
+app.use('/api/realTimeProducts', realTimeProductsRouter)
+
+const httpServer = app.listen(8080, () => {
+    console.log('Servidor listo para ser usado!');
+});
+
+const wsServer = new Server(httpServer);
+app.set('ws', wsServer)
+
+wsServer.on('connection', (socket) => {
+    console.log('Nuevo cliente conectado via WebSocket');
+    
+})
